0d330a46f34d8d809585a46951e1d9ffe201df87
Ilia Filimonov <9991571155i@gmail.com>
2023-10-25 22:45:58 +0700

    Поправит пути к изображениям (находятся в public). Поправит отступы в коде

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,57 +1,57 @@
-import {data} from "./data";
-import Swiper from "swiper"; // Инициализация слайдера
-import {Navigation, Pagination} from "swiper/modules"; // Инициализация иконок, пагинации и т.д.
+import { data } from './data'
+import Swiper from 'swiper' // Инициализация слайдера
+import { Navigation, Pagination } from 'swiper/modules' // Инициализация иконок, пагинации и т.д.
 
 const GLOBAL = {
-    currentPage: window.location.pathname,
-};
+  currentPage: window.location.pathname,
+}
 
 // Функция отрисовки карточек
 const renderData = () => {
-    const swiperWrapper = document.querySelector(".swiper-wrapper");
+  const swiperWrapper = document.querySelector('.swiper-wrapper')
 
-    data.forEach(function (product) {
-        const div = document.createElement("div"); // Создать дивку
-        div.classList.add("swiper-slide"); // Добавить класс дивке
+  data.forEach(function (product) {
+    const div = document.createElement('div') // Создать дивку
+    div.classList.add('swiper-slide') // Добавить класс дивке
 
-        div.innerHTML = `
-            <a
-                class="tasks-link"
-                href="tasks-details.html?id=${product.id}"
-                target="_blank"
-                rel="noopener noreferrer nofollow"
-            >
-                <div class="tasks-card">
-                    <div class="skeleton tasks-card__image">
-                        <img src="${product.imgSrc}" alt="${product.name}"/>
-                    </div>
-                </div>
-            </a>
-        `;
+    div.innerHTML = `
+      <a
+        class="tasks-link"
+        href="tasks-details.html?id=${product.id}"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+      >
+        <div class="tasks-card">
+          <div class="skeleton tasks-card__image">
+            <img src="${product.imgSrc}" alt="${product.name}"/>
+          </div>
+        </div>
+      </a>
+    `
 
-        swiperWrapper.appendChild(div); // Вставка сформированной дивки в swiperWrapper
+    swiperWrapper.appendChild(div) // Вставка сформированной дивки в swiperWrapper
 
-        initTasksSwiper(); // Вызов функции для инициализации слайдера
-    });
-};
+    initTasksSwiper() // Вызов функции для инициализации слайдера
+  })
+}
 
-renderData();
+renderData()
 
 /* Слайдер tasks */
 function initTasksSwiper() {
-    // eslint-disable-next-line no-unused-vars
-    const swiper = new Swiper(".swiper", {
-        modules: [Navigation, Pagination],
-        direction: "horizontal",
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+  // eslint-disable-next-line no-unused-vars
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    direction: 'horizontal',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })
 }
 
 // Инициализация перехода по страницам
@@ -69,19 +69,19 @@ function initTasksSwiper() {
 // document.addEventListener("DOMContentLoaded", init);
 
 // Skeleton
-window.addEventListener("load", () => {
-    const skeletons = document.querySelectorAll(".skeleton");
+window.addEventListener('load', () => {
+  const skeletons = document.querySelectorAll('.skeleton')
 
-    skeletons.forEach(function (element) {
-        element.classList.remove("skeleton");
-    });
-});
+  skeletons.forEach(function (element) {
+    element.classList.remove('skeleton')
+  })
+})
 
 // Иконки heart /
-const hearts = document.querySelectorAll(".card-heart");
+const hearts = document.querySelectorAll('.card-heart')
 
 hearts.forEach((heart) => {
-    heart.addEventListener("click", () => {
-        heart.classList.toggle("heart-active");
-    });
-});
+  heart.addEventListener('click', () => {
+    heart.classList.toggle('heart-active')
+  })
+})
